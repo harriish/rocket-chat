f3dab2f499ec736f3a4347f658da08d524f1dcd1
harriish <harishkumarivaturi@gmail.com>
2021-04-26 20:30:22 +0200

    updated main.tf and removed ssh provisioned

--- main.tf
+++ main.tf
@@ -8,18 +8,6 @@ resource "aws_instance" "my-instance" {
   key_name               = "deployer-key"
   subnet_id              = aws_subnet.My_VPC_Subnet.id
   vpc_security_group_ids = [aws_security_group.My_VPC_Security_Group.id]
-  provisioner "remote-exec" {
-    inline = ["echo 'hello'"]
-
-    connection {
-      type = "ssh"
-      user = "ubuntu"
-      host = aws_instance.my-instance.public_ip
-    }
-  }
-
-  provisioner "local-exec" {
-     command = "ansible-playbook -u ubuntu -i ${aws_instance.my-instance.public_ip}, play.yml"
-    }  
+  
 }
 
