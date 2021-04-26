f753298f41c4fe1f3f0e551b1ea1b3234e72e731
ivaturi venkata harish kumar <43529736+harriish@users.noreply.github.com>
2021-04-26 22:57:37 +0200

    Update docker_compose_start.sh

--- docker_compose_start.sh
+++ docker_compose_start.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Install Docker
+sudo yum install -y docker docker-compose git
+sudo service docker start
+sleep 10
+
+# Install docker-compose
+sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+sudo chmod +x /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/RocketChat/Rocket.Chat/develop/docker-compose.yml -o docker-compose.yml
-docker-compose up -d
+sudo docker-compose up -d
