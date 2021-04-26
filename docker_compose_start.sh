21afc11d53e3c781644788a2afb6e056bbdd0bba
harriish <harishkumarivaturi@gmail.com>
2021-04-26 20:21:07 +0200

    added all files

---
+++ docker_compose_start.sh
@@ -0,0 +1,2 @@
+curl -L https://raw.githubusercontent.com/RocketChat/Rocket.Chat/develop/docker-compose.yml -o docker-compose.yml
+docker-compose up -d
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+terraform destroy
+docker-compose down
